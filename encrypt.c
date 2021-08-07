8dba111f23da8dcd4be64d0309e206e2647e4413
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-07 19:45:33 +0100

    refactor(encrypt.c): Encrypt a file

--- encrypt.c
+++ encrypt.c
@@ -1,42 +1,50 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define ALPHABET_SIZE (26)
 #define ALPHABET_MIN (97)
 #define ALPHABET_MAX (122)
 
-char *encrypt(const char *plain_text, const int shift)
+void encrypt(char *text, const int shift)
 {
-  char *cipher_text = malloc(sizeof(plain_text));
-  for (int i = 0; i < strlen(plain_text); i++)
+  for (int i = 0; i < strlen(text); i++)
   {
-    int ascii = plain_text[i];
+    int ascii = text[i];
     if (ALPHABET_MIN <= ascii && ascii <= ALPHABET_MAX)
     {
       if (ascii + shift > ALPHABET_MAX)
       {
-        cipher_text[i] = ascii + shift - ALPHABET_SIZE;
+        text[i] = ascii + shift - ALPHABET_SIZE;
       }
       else
       {
-        cipher_text[i] = plain_text[i] + shift;
+        text[i] = text[i] + shift;
       }
     }
     else
     {
-      cipher_text[i] = plain_text[i];
+      text[i] = text[i];
     }
   }
-  return cipher_text;
 }
 
 int main(int argc, char const *argv[])
 {
-  const char *plain_text = argv[1];
-  const int shift = atoi(argv[2]);
-  char *cipher_text = encrypt(plain_text, shift);
-  printf("Encrypted text: %s\n", cipher_text);
-  free(cipher_text);
+  FILE *fp = fopen(argv[1], "r");
+  int shift = atoi(argv[2]);
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+  rewind(fp);
+  char *fcontent = malloc(size);
+  fread(fcontent, 1, size, fp);
+  fclose(fp);
+  fp = fopen(argv[3], "w");
+  encrypt(fcontent, shift);
+  fwrite(fcontent, 1, size, fp);
+  fclose(fp);
+  free(fcontent);
   return 0;
 }
