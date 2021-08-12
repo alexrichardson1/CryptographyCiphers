4991bcc9fa6424f19e9944f39a9276b68f328b8b
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-12 12:40:05 +0100

    fix(cryptography.h): Update `brute_force`

--- cryptography.h
+++ cryptography.h
@@ -6,6 +6,6 @@
 
 extern void ceasar_cipher(FILE *fp_input, FILE *fp_output, const int shift, bool encrypt);
 extern void vigenere_cipher(FILE *fp_input, FILE *fp_output, const char *key, bool encrypt);
-extern void brute_force(void);
+extern void brute_force(FILE *fp_input, FILE *fp_output);
 
 #endif
