5bd00434da4a1f21b430a0a8980be0081fc525b3
R, Alex <alex.richardson19@imperial.ac.uk>
2021-08-12 12:38:12 +0100

    refactor(cryptography.h): Declare `_cipher`s

--- cryptography.h
+++ cryptography.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-extern void ceasar(FILE *fp_input, FILE *fp_output, bool encrypt);
-extern void vigenere(FILE *fp_input, FILE *fp_output, bool encrypt);
+extern void ceasar_cipher(FILE *fp_input, FILE *fp_output, const int shift, bool encrypt);
+extern void vigenere_cipher(FILE *fp_input, FILE *fp_output, const char *key, bool encrypt);
 extern void brute_force(void);
 
 #endif
